e3ec59e4eae618bb0628f5d17bd75a5becd35fce
Stefan Heid <stefan@heid.ws>
2016-04-01 23:14:28 +0200

    hange db_version

--- install/froxlor.sql
+++ install/froxlor.sql
@@ -558,7 +558,7 @@ INSERT INTO `panel_settings` (`settinggroup`, `varname`, `value`) VALUES
 	('panel', 'password_special_char_required', '0'),
 	('panel', 'password_special_char', '!?<>§$%+#=@'),
 	('panel', 'version', '0.9.35-rc1'),
-	('panel', 'db_version', '201603150');
+	('panel', 'db_version', '201603151');
 
 
 DROP TABLE IF EXISTS `panel_tasks`;
