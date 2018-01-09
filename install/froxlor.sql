1fd28ad2949598900c452045428470b8f627ba07
Michael Kaufmann (d00p) <d00p@froxlor.org>
2018-01-09 14:50:52 +0100

    make it a setting to switch between ACME v1 and v2
    Signed-off-by: Michael Kaufmann (d00p) <d00p@froxlor.org>

--- install/froxlor.sql
+++ install/froxlor.sql
@@ -536,6 +536,7 @@ INSERT INTO `panel_settings` (`settinggroup`, `varname`, `value`) VALUES
 	('system', 'letsencryptkeysize', '4096'),
 	('system', 'letsencryptreuseold', 0),
 	('system', 'leenabled', '0'),
+	('system', 'leapiversion', '1'),
 	('system', 'backupenabled', '0'),
 	('system', 'dnsenabled', '0'),
 	('system', 'dns_server', 'bind'),
@@ -597,7 +598,7 @@ INSERT INTO `panel_settings` (`settinggroup`, `varname`, `value`) VALUES
 	('panel', 'password_special_char', '!?<>§$%+#=@'),
 	('panel', 'customer_hide_options', ''),
 	('panel', 'version', '0.9.38.8'),
-	('panel', 'db_version', '201801080');
+	('panel', 'db_version', '201801090');
 
 
 
--- install/updates/froxlor/0.9/update_0.9.inc.php
+++ install/updates/froxlor/0.9/update_0.9.inc.php
@@ -3845,3 +3845,12 @@ if (isDatabaseVersion('201801070')) {
 
 	updateToDbVersion('201801080');
 }
+
+if (isDatabaseVersion('201801080')) {
+
+	showUpdateStep("Adding new setting for Let's Encrypt ACME version");
+	Settings::AddNew('system.leapiversion', '1');
+	lastStepStatus(0);
+
+	updateToDbVersion('201801090');
+}
