9b536b0d11b6447ce4bba6564b1922be3dcac43c
Stefan Heid <stefan@heid.ws>
2018-01-15 17:53:57 +0100

    add setting to disable LE self-check; set version to 0.9.38.8 for maintenance/bugfix release
    Signed-off-by: Michael Kaufmann (d00p) <d00p@froxlor.org>

--- install/updates/froxlor/0.9/update_0.9.inc.php
+++ install/updates/froxlor/0.9/update_0.9.inc.php
@@ -3753,3 +3753,16 @@ if (isDatabaseVersion('201708020')) {
 
 	updateToDbVersion('201708240');
 }
+
+if (isDatabaseVersion('201708240')) {
+	
+	showUpdateStep("Adding new 'disable LE self-check' setting");
+	$system_disable_le_selfcheck = isset($_POST['system_disable_le_selfcheck']) ? (int) $_POST['system_disable_le_selfcheck'] : 0;
+	Settings::AddNew('system.disable_le_selfcheck', $system_disable_le_selfcheck);
+	lastStepStatus(0);
+
+	updateToDbVersion('201712310');
+
+	showUpdateStep("Updating from 0.9.38.7 to 0.9.38.8", false);
+	updateToVersion('0.9.38.8');
+}
